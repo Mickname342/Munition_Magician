281e39cff79d9c4cf907360d114330c4149e53b6
CabeMax1 <C21486482@mytudublin.ie>
2023-03-17 15:01:21 +0000

    Main plus pause menu
    (Very rough)

---
+++ Roguelike-GameDesig/Assets/Scripts/pauseManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseManager : MonoBehaviour
+{
+    public bool paused = false;
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        paused = !paused;
+        if (paused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
